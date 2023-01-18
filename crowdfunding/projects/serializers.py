2e487783249f45bdca8328be86359959dd2cc5be
Firzana-Alice <114482681+Firzana-Alice@users.noreply.github.com>
2023-01-18 21:32:35 +0800

    GRB 1 & 2
    GRB 1 AND 2 COMPLETED - PROJECT SET UP AND HANDLING MODEL RELATIONSHIPS

---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Project, Pledge
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField(max_length=None)
+    goal = serializers.IntegerField()
+    image = serializers.URLField()
+    is_open = serializers.BooleanField()
+    date_created = serializers.DateTimeField()
+
+    
+    def create (self,validated_data ):
+        return Project.objects.create(**validated_data)
+
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pledge
+        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
